9a25999f3ae3d6fe9957fceb9e04969f80d13c30
sobanya <shadihanova.anna@yandex.ru>
2025-08-09 15:36:03 +0300

    second sprint

---
+++ task_2.py
@@ -0,0 +1,34 @@
+class Movies:
+    def __init__(self):
+        self.movies = []
+
+    def add_movie(self, movie):
+        self.movies.append(movie)
+
+
+class Comedy(Movies):
+    def __init__(self):
+        super().__init__()
+
+    def add_movie(self, movie):
+        self.movies.append(movie)
+        return f"Комедии: {self.movies}"
+
+
+class Drama(Movies):
+    def __init__(self):
+        super().__init__()
+
+    def add_movie(self, movie):
+        self.movies.append(movie)
+        return f"Драмы: {self.movies}"
+
+
+c = Comedy()
+d = Drama()
+
+comedies = c.add_movie("Большой Куш")
+print(comedies)
+
+dramas = d.add_movie("Оружейный барон")
+print(dramas)
---
+++ task_4.py
@@ -0,0 +1,27 @@
+class EmployeeSalary:
+    hourly_payment = 400
+
+    def __init__(self, name, hours, rest_days, email):
+        self.name = name
+        self.hours = hours
+        self.rest_days = rest_days
+        self.email = email
+
+    @classmethod
+    def get_hours(cls, name, hours=None, rest_days=None, email=None):
+        if hours is None and rest_days is not None:
+            hours = 8 * (7 - rest_days)
+        return cls(name, hours, rest_days, email)
+
+    @classmethod
+    def get_email(cls, name, hours=None, rest_days=None, email=None):
+        if not email:
+            email = f"{name}@email.com"
+        return cls(name, hours, rest_days, email)
+
+    @classmethod
+    def set_hourly_payment(cls, new_payment):
+        cls.hourly_payment = new_payment
+
+    def salary(self):
+        return self.hours * self.hourly_payment
